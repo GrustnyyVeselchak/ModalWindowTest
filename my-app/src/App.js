15398133ecc09ac43a359369a1c4da8bd3012ab4
Emil <113785577+GrustnyyVeselchak@users.noreply.github.com>
2023-03-02 22:44:00 +0400

    модальное окно (сырое)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,43 @@
 import React from 'react';
-import {Button, Modal, Box, Typography} from '@mui/material';
+import {Button, Modal, Box, Typography, Container} from '@mui/material'
 
-export default function AddButton(props) {
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  height: 600,
+  width: 800,
+  bgcolor: 'background.paper',
+  border: '1px solid #000',
+  boxShadow: 1,
+  p: 5,
+};
+
+export default function (props) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <div>
-      <Button onClick={handleOpen}>Open modal</Button> 
+    <Container>
+      <Button onClick={handleOpen}>Open modal</Button>
       <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
-      ></Modal>     
-    </div>
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Суды пихат заголовок
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            суды пихат опысаные
+          </Typography>
+        </Box>
+      </Modal>
+    </Container>
+    
   )
 }
-
-/* Модуль из
-<Button onClick={handleOpen}>Open modal</Button>
-<Modal
-  open={open}
-  onClose={handleClose}
-  aria-labelledby="modal-modal-title"
-  aria-describedby="modal-modal-description"
->
-  <Box sx={style}>
-    <Typography id="modal-modal-title" variant="h6" component="h2">
-      Text in a modal
-    </Typography>
-    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-      Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-    </Typography>
-  </Box>
-</Modal>
-*/
-
-
-/*Кнопка отправить
-<Button variant="contained" endIcon={<SendIcon />}>
-  Send
-</Button> */
